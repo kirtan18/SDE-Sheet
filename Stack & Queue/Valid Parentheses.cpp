0ecc7675bb5bad8d3931c3e78459fcb5b67ec13d
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-11-10 10:16:37 +0530

    Valid Parentheses [Easy]

---
+++ Stack & Queue/Valid Parentheses.cpp
@@ -0,0 +1,44 @@
+https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        char x;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+                continue;
+            }
+            
+            if(st.empty()){
+                return false;
+            }
+            
+            switch(s[i]){
+                    
+                case ')':
+                    x = st.top();
+                    st.pop();
+                    if(x == '{' || x == '[')
+                        return false;
+                    break;
+                    
+                case '}':
+                    x = st.top();
+                    st.pop();
+                    if(x == '[' || x == '(')
+                        return false;
+                    break;
+                case ']':
+                    x = st.top();
+                    st.pop();
+                    if(x == '(' || x == '{')
+                        return false;
+                    break;
+            }
+        }
+        return st.empty();
+    }
+};
