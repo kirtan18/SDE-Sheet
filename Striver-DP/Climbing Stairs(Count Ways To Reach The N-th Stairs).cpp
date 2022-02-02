af64d68d0ee713271b0893f060d0258b5f6215cb
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2022-02-02 23:35:57 +0530

    Climbing Stairs (Count Ways To Reach The N-th Stairs) [1D-DP] [MEdium]

---
+++ Striver-DP/Climbing Stairs(Count Ways To Reach The N-th Stairs).cpp
@@ -0,0 +1,40 @@
+CN : https://www.codingninjas.com/codestudio/problems/count-ways-to-reach-nth-stairs_798650?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+LC : https://leetcode.com/problems/climbing-stairs/
+
+
+/////////////////////////////////////////////////// Tabulation || T.C = O(N) || S.C = O(N) //////////////////////////////////////////
+
+int main() {
+
+  int n=3;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 1;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return 0;
+}
+
+
+/////////////////////////////////////////////////// Memory- Optimization || T.C = O(N) || S.C = O(1) /////////////////////////////////////////////
+
+
+int main() {
+
+  int n=3;
+  
+  int prev2 = 1;
+  int prev = 1;
+  
+  for(int i=2; i<=n; i++){
+      int cur_i = prev2+ prev;
+      prev2 = prev;
+      prev= cur_i;
+  }
+  cout<<prev;
+  return 0;
+}
